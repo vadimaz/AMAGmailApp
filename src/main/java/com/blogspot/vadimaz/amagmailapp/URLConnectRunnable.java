28f33b314e3f4909c2b77c9a230421996bd0fa55
vzorenko@gmail.com <Airpilot1979>
2019-10-05 22:19:31 +0300

    2.5.1

--- src/main/java/com/blogspot/vadimaz/amagmailapp/URLConnectRunnable.java
+++ src/main/java/com/blogspot/vadimaz/amagmailapp/URLConnectRunnable.java
@@ -1,8 +1,6 @@
 package com.blogspot.vadimaz.amagmailapp;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
